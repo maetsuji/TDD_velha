d3258cd268f0025afb77e41e33e182bbcfd4bf95
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-06-11 20:04:02 -0300

    Teste 2 - Implementado

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -5,6 +5,11 @@
 
 TEST_CASE( "Jogos da Velha", "[jogos]" ) {
     // Implementar testes aqui:
-    int tabuleiro[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-    REQUIRE( valida_velha(tabuleiro) == -1); // Indefinido
+    int tabuleiro[3][3] = {{0,0,0},{0,0,0},{0,0,0}}; // Teste 1
+    int resultado = -1  // Indefinido
+    REQUIRE( valida_velha(tabuleiro) == resultado);
+    
+    int tabuleiro[3][3] = {{1,1,1},{0,0,0},{2,2,0}};// Teste 2
+    resultado = 1;  // Jogador 1 venceu
+    REQUIRE( valida_velha(tabuleiro) == resultado);
 }
