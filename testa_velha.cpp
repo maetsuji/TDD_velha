0e39bfac064bc2761ae193b310d1300c0688b3e8
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-06-11 22:19:59 -0300

    Teste 7 - ⚙️ Implementado

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -20,6 +20,10 @@ TEST_CASE( "Jogos da Velha", "[jogos]" ) {
     int tabuleiro4[3][3] = {{1,1,2},{2,1,1},{1,2,2}};    // Teste 4
     int resultado4 = 0 ; // Empate
     REQUIRE( valida_velha(tabuleiro4) == resultado4);
+
+    int tabuleiro7[3][3] = {{1,2,1},{2,1,1},{2,2,1}};    // Teste 7
+    int resultado7 = 1; // permitido
+    REQUIRE( valida_velha(tabuleiro7) == resultado7);
 }
 
 TEST_CASE( "Jogos da Velha Invalidos", "[jogos]" ) {
@@ -31,7 +35,21 @@ TEST_CASE( "Jogos da Velha Invalidos", "[jogos]" ) {
     int resultado5b = -2; // Proibido, dois jogadores venceram na horizontal
     REQUIRE( valida_velha(tabuleiro5b) == resultado5b);
 
-    int tabuleiro6[3][3] = {{2,0,1},{2,0,1},{2,0,1}};    // Teste 5
+    int tabuleiro6[3][3] = {{2,0,1},{2,0,1},{2,0,1}};    // Teste 6
     int resultado6 = -2; // Proibido, dois jogadores venceram na vertical
     REQUIRE( valida_velha(tabuleiro6) == resultado6);
+
+    int tabuleiro8[3][3] = {{1,1,1},{1,1,1},{1,1,1}};    // Teste 8
+    int resultado8 = -2; // tudo x
+    REQUIRE( valida_velha(tabuleiro8) == resultado8);
+}
+
+TEST_CASE( "Mais Jogos da Velha", "[jogos]" ) {
+    int tabuleiro9[3][3] = {{0,0,0},{0,0,0},{0,0,0}};    // Teste 9
+    int resultado9 = -1; // Indefinido
+    REQUIRE( valida_velha(tabuleiro9) == resultado9);
+
+    int tabuleiro10[3][3] = {{1,1,1},{0,99,0},{2,2,0}};    // Teste 10
+    int resultado10 = -2; // Proibido, valor inválido (99)
+    REQUIRE( valida_velha(tabuleiro10) == resultado10);
 }
--- velha.cpp
+++ velha.cpp
@@ -1,121 +1,123 @@
 #include "velha.hpp"
-#include <cstring> // For strlen
 
-int conta_ocorrencia(int matriz[3][3], int VOX) { // Recebe uma matriz de inteiros com 3 linhas e 3 colunas
-    // Esta função conta quantos X e O existem na matriz.
-    int contagem = 0; // Inicializa o contador de ocorrências
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (matriz[i][j] == VOX) {
+// Conta quantas vezes um valor (0, 1 ou 2) aparece na matriz
+int conta_ocorrencia(int matriz[3][3], int VOX) {
+    int contagem = 0;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (matriz[i][j] == VOX)
                 contagem++;
-            }
-        }
-    }
-    return contagem; // Retorna o número de ocorrências de Vazios, X ou O
+    return contagem;
 }
-int valida_velha(int matriz[3][3]) { // Recebe uma matriz de inteiros com 3 linhas e 3 colunas
+
+// Verifica se há algum valor inválido na matriz (não 0, 1, ou 2)
+// bool contem_valor_invalido(int matriz[3][3]) {
+//     for (int i = 0; i < 3; i++)
+//         for (int j = 0; j < 3; j++)
+//             if (matriz[i][j] < 0 || matriz[i][j] > 2)
+//                 return true;
+//     return false;
+// }
+
+// Retorna o vencedor (1 para X, 2 para O) ou 0 se não houver
+int verifica_vencedor(int matriz[3][3]) {
+    // Linhas
+    for (int i = 0; i < 3; i++)
+        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != 0)
+            return matriz[i][0];
+
+    // Colunas
+    for (int j = 0; j < 3; j++)
+        if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j] && matriz[0][j] != 0)
+            return matriz[0][j];
+
+    // Diagonal principal
+    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != 0)
+        return matriz[0][0];
+
+    // Diagonal secundária
+    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != 0)
+        return matriz[0][2];
+
+    return 0;
+}
+
+// Verifica se há mais de um vencedor
+bool dois_vencedores(int matriz[3][3]) {
+    int cont = 0;
+    int vencedor_atual = 0;
+
+    // Linhas
+    for (int i = 0; i < 3; i++)
+        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != 0)
+            if (vencedor_atual == 0)
+                vencedor_atual = matriz[i][0];
+            else if (vencedor_atual != matriz[i][0])
+                return true;
+
+    // Colunas
+    for (int j = 0; j < 3; j++)
+        if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j] && matriz[0][j] != 0)
+            if (vencedor_atual == 0)
+                vencedor_atual = matriz[0][j];
+            else if (vencedor_atual != matriz[0][j])
+                return true;
+
+    // Diagonais
+    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != 0)
+        if (vencedor_atual == 0)
+            vencedor_atual = matriz[0][0];
+        else if (vencedor_atual != matriz[0][0])
+            return true;
+
+    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != 0)
+        if (vencedor_atual == 0)
+            vencedor_atual = matriz[0][2];
+        else if (vencedor_atual != matriz[0][2])
+            return true;
+
+    return false;
+}
+
+int valida_velha(int matriz[3][3]) {
     // O jogo da velha é representado como uma matriz 3x3 de inteiros. 
     // O valor 0 significa que a posição está vazia 
     // O valor 1 significa que a posição está com um X 
     // O valor 2 significa que a posição está com um O
     
-    int resultado = -1; // Inicializa o resultado como indefinido, deixando para acertar a lógica depois
-    int vencedorFlag = 0; // Inicializa o vencedor final como 0 (nenhum vencedor)
+    // if (contem_valor_invalido(matriz))
+    //     return -2;
 
-    // Percorre a matriz para contar quantos X, O e posições vazias existem
-    // Isso é importante para determinar o estado do jogo
-    // e se o jogo é válido ou não.
-    int contagemX = conta_ocorrencia(matriz, 1); // Contador de X
-    int contagemO = conta_ocorrencia(matriz, 2); // Contador de O
-    int contagemVazio = conta_ocorrencia(matriz, 0); // Contador de posições vazias
+    int contagemX = conta_ocorrencia(matriz, 1);
+    int contagemO = conta_ocorrencia(matriz, 2);
+    int contagemVazio = conta_ocorrencia(matriz, 0);
 
-    // Verifica se há um vencedor:
-    if (contagemX < contagemO || contagemX > contagemO + 1) {
-        // Se a quantidade de X é menor que a de O ou maior que a de O + 1, o jogo é impossível
-        resultado = -2; // Jogo impossível pelas regras
-    } else {
-        // Verifica linhas
-        if (!vencedorFlag) {
-            for (int i = 0; i < 3; i++) {
-                if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]) {
-                    if (matriz[i][0] == 1) {
-                        if (vencedorFlag) {
-                            return -2; // Se já encontrou um vencedor, mas ainda está verificando, é inválido
-                        } else {
-                            vencedorFlag = 1; // Marca que já encontrou um vencedor
-                            resultado = 1; // O venceu
-                        }
-                    } else if (matriz[i][0] == 2) {
-                        if (vencedorFlag) {
-                            return -2; // Se já encontrou um vencedor, mas ainda está verificando, é inválido
-                        } else {
-                            vencedorFlag = 1; // Marca que já encontrou um vencedor
-                            resultado = 2; // O venceu
-                        }
-                    }
-                }
-            }
-        }
-                // Verifica colunas
-        if (!vencedorFlag) {            
-            for (int j = 0; j < 3; j++) {
-                if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j]) {
-                    if (matriz[0][j] == 1) {
-                        if (vencedorFlag) {
-                            resultado = -2; // Se já encontrou um vencedor, mas ainda está verificando, é inválido
-                        } else {
-                            vencedorFlag = 1; // Marca que já encontrou um vencedor
-                            resultado = 1; // X venceu
-                        }
-                    } else if (matriz[0][j] == 2) {
-                        if (vencedorFlag) {
-                            return -2; // Se já encontrou um vencedor, mas ainda está verificando, é inválido
-                        } else {
-                            vencedorFlag = 1; // Marca que já encontrou um vencedor
-                            resultado = 2; // O venceu
-                        }
-                    }
-                }
-            }
-        }
-        // Verifica diagonal principal 
-        if (!vencedorFlag) { 
-            if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2]) {
-                if (matriz[0][0] == 1) {
-                    resultado = 1; // X venceu
-                    vencedorFlag = 1; // Marca que já encontrou um vencedor
-                } else if (matriz[0][0] == 2) {
-                    resultado = 2; // O venceu
-                    vencedorFlag = 1; // Marca que já encontrou um vencedor
-                }
-            }
-        }
-        // Verifica diagonal secundária
-        if (!vencedorFlag) { 
-            if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0]) {
-                if (matriz[0][2] == 1) {
-                    resultado = 1; // X venceu
-                    vencedorFlag = 1; // Marca que já encontrou um vencedor
-                } else if (matriz[0][2] == 2) {
-                    resultado = 2; // O venceu
-                    vencedorFlag = 1; // Marca que já encontrou um vencedor
-                }
-            }
-        }
-        if (!vencedorFlag) {
-            // Se não houve vencedor, verifica se o jogo está empatado
-            if (contagemVazio == 0) {
-                resultado = 0; // Empate
-            } else {
-                resultado = -1; // Jogo indefinido (ex. tem apenas um X)
-            }
-        }
-    }
+    if (contagemX < contagemO || contagemX > contagemO + 1)
+        return -2;
 
-    return resultado; 
+    if (dois_vencedores(matriz))
+        return -2;
+
+    int vencedor = verifica_vencedor(matriz);
+
+    if (vencedor == 1 && contagemX != contagemO + 1)
+        return -2;
+
+    if (vencedor == 2 && contagemX != contagemO)
+        return -2;
+
+    if (vencedor != 0)
+        return vencedor;
+
+    if (contagemVazio == 0)
+        return 0; // empate
+
+    return -1; // jogo indefinido
+
     // Retorna 1 se o vencedor foi o X 
     // Retorna 2 se o vencedor foi o O 
     // Retorna 0 se o jogo está empatado 
     // Retorna -1 se o jogo está indefinido (ex. tem apenas um X) 
     // Retorna -2 se o jogo é com certeza impossível pelas regras (ex. todas as posições são X) 
+
 }
