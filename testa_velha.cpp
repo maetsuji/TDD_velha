73b918ab7d1702b5fec28f6a2d8daa7f28aabf99
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-06-11 21:19:23 -0300

    Teste 3 - ⚙️ Implementado

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -12,4 +12,8 @@ TEST_CASE( "Jogos da Velha", "[jogos]" ) {
     int tabuleiro2[3][3] = {{1,1,1},{0,0,0},{2,2,0}};    // Teste 2
     int resultado2 = 1;  // Jogador 1 venceu
     REQUIRE( valida_velha(tabuleiro2) == resultado2);
+
+    int tabuleiro3[3][3] = {{2,0,0},{2,0,0},{2,1,1}};    // Teste 3
+    int resultado3 = -2; // Proibido, Quantidade de O é maior que a de X
+    REQUIRE( valida_velha(tabuleiro3) == resultado3);
 }
